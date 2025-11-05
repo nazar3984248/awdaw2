30ff7cea423a8f2cc4048a4cc13f063a1f7da27d
nazar3984248 <fonyfo467@gmail.com>
2025-11-05 20:52:47 +0200

    wdqwd

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-from flask import Flask, request, jsonify, render_template
-import requests
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Укажите ваш Telegram Bot Token и chat_id
-TELEGRAM_API_URL = "https://api.telegram.org/bot<8489651453:AAF-9pbgHvlHpbJ8gbXMvXAHEhpEC4a_s_U>/sendMessage"
-CHAT_ID = "<@xfxki>"
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    data = request.get_json()  # Получаем данные из POST запроса
-    message = data.get('message')  # Извлекаем сообщение
-    send_to_telegram(message)  # Отправляем сообщение в Telegram
-    return jsonify({"status": "success"})
-
-def send_to_telegram(message):
-    payload = {
-        'chat_id': CHAT_ID,
-        'text': message
-    }
-    response = requests.post(TELEGRAM_API_URL, data=payload)
-    if response.status_code != 200:
-        print(f"Error sending message: {response.text}")
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()  # Получаем данные в формате JSON
+    user_input = data.get('user_input')  # Извлекаем текст
+    print(f'Полученный текст: {user_input}')  # Выводим на сервере
+    return jsonify({"response": user_input})  # Отправляем ответ обратно в формате JSON
 
 if __name__ == '__main__':
     app.run(debug=True)
